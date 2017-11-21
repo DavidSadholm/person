d4e3d3f4ca937353266fd801d805eb78c9c75b42
DavidSadholm <david.sadholm@gmail.com>
2017-11-21 10:39:24 +0100

    Skapat ett namn Stefan Zethraeus

---
+++ src/person/Person.java
@@ -0,0 +1,19 @@
+package person;
+
+public class Person {
+
+	private String firstname;
+
+	private String lastname;
+
+	public Person(String name) {
+
+	firstname = "Stefan";
+		
+	lastname = "Zethraeus";
+	
+				
+				
+	}
+
+}
